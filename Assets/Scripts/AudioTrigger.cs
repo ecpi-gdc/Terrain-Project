45feb501d6686da84d508638f3aab3f8b25c6afe
Frank Van Sant <fvansant@ecpi.edu>
2019-06-28 10:55:01 -0400

    added audio and camera switcher

---
+++ Assets/Scripts/AudioTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTrigger : MonoBehaviour {
+	private AudioSource audioPlayer;
+
+	// Start is called before the first frame update
+	void Start() {
+		audioPlayer = GetComponent<AudioSource>();
+
+	}
+
+	// Update is called once per frame
+	void Update() { }
+
+	private void OnTriggerEnter(Collider other) {
+		if (other.CompareTag("MainCamera")) {
+			print("Played!");
+			audioPlayer.PlayOneShot(audioPlayer.clip);
+		}
+	}
+
+}
